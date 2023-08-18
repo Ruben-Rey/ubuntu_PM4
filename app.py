7fc79f73896515dc7d75a9441e3ae35da0294e1c
ruben campos reymundis <ibiza.reymundis@gmail.com>
2023-08-18 12:16:37 -0500

    actualizacion

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import time
 
-carpeta = "/home/ibiza/.pm2/logs/bot-prueba-out.log"
 while True:
-    with open(carpeta, "a") as log_file:
-        print("Enviando informacion a ......")
-        log_file.write("Enviando informacion a ......\n")
+    print("Enviando informacion a ...pm1")
     
     time.sleep(0.1)  # Esperar 1 segundo entre cada registro
